9e41071bc0902649b321f0c3d2956cf19df90b4c
David Gulyas <gulyman@gmail.com>
2022-12-29 15:41:29 -0700

    Rewrite Adder and add tests

--- GateSim/Arithmetic/Adder.cs
+++ GateSim/Arithmetic/Adder.cs
@@ -4,24 +4,61 @@ namespace GateSim.Arithmetic
     {
         public bool[] Input1 { get; }
         public bool[] Input2 { get; }
+        //public bool[] CarryIn {get; }
+        public bool[] CarryOut { get; }
         public bool[] Output { get; }
         public int Id { get; set; }
 
-        public bool Tick()
-        { //This isn't a great implementation. There are probably a lot
-          //of edge cases around converting to int and back again.
-          //Does this work for 2's complement negatives?
+        // public bool Tick()
+        // { //This isn't a great implementation. There are probably a lot
+        //   //of edge cases around converting to int and back again.
+        //   //Does this work for 2's complement negatives?
+        //     var oldState = (bool[]) Output.Clone();
+
+        //     var a = Input1.ToInt();
+        //     var b = Input2.ToInt();
+
+        //     var c = a+b;
+        //     if(c > Math.Pow(2,Output.Length)){ //Is this right?
+        //         c = (int)Math.Pow(2,Output.Length); //It might need to be "- 1"
+        //     }
+
+        //     Util.SetArrayToValues(Output, c.ToBoolArray(Output.Length));
+
+        //     return !oldState.SequenceEqual(Output);
+        // }
+
+        public bool Tick(){
             var oldState = (bool[]) Output.Clone();
 
-            var a = Input1.ToInt();
-            var b = Input2.ToInt();
-
-            var c = a+b;
-            if(c > Math.Pow(2,Output.Length)){ //Is this right?
-                c = (int)Math.Pow(2,Output.Length); //It might need to be "- 1"
+            var sumArray = new bool[Input1.Length];
+            bool carry = false;
+            for(int i = 0; i < sumArray.Length; i++)
+            {
+                var sum = (carry ? 1: 0) + (Input1[i] ? 1 : 0) + (Input2[i] ? 1 : 0);
+                if(sum == 0){
+                    sumArray[i] = false;
+                    carry = false;
+                }
+                else if(sum == 1)
+                {
+                    sumArray[i] = true;
+                    carry = false;
+                }
+                else if(sum == 2)
+                {
+                    sumArray[i] = false;
+                    carry = true;
+                }
+                else if(sum == 3)
+                {
+                    sumArray[i] = true;
+                    carry = true;
+                }
             }
 
-            Util.SetArrayToValues(Output, c.ToBoolArray(Output.Length));
+            CarryOut[0] = carry;
+            Util.SetArrayToValues(Output, sumArray);
 
             return !oldState.SequenceEqual(Output);
         }
@@ -30,6 +67,7 @@ namespace GateSim.Arithmetic
             Input1 = new bool[bitWidth];
             Input2 = new bool[bitWidth];
             Output = new bool[bitWidth];
+            CarryOut = new bool[1];
         }
     }
 }
---
+++ Tests/Arithmetic/AdderTests.cs
@@ -0,0 +1,27 @@
+using GateSim;
+using GateSim.Arithmetic;
+
+namespace Tests.Arithmetic
+{
+    [TestClass]
+    public class AdderTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 1, 2, 0)]
+        [DataRow(3, 100, 103, 0)]
+        [DataRow(123, 123, 246, 0)]
+        [DataRow(255, 1, 0, 1)]
+        [DataRow(255, 255, 254, 1)]
+        public void AdderDoesThing(int a, int b, int sum, int carryOut){
+            var bitWidth = 8;
+            var adder = new Adder(bitWidth);
+            Util.SetArrayToValues(adder.Input1, a.ToBoolArray(bitWidth));
+            Util.SetArrayToValues(adder.Input2, b.ToBoolArray(bitWidth));
+
+            adder.Tick();
+
+            CollectionAssert.AreEqual(sum.ToBoolArray(bitWidth), adder.Output);
+            CollectionAssert.AreEqual(carryOut.ToBoolArray(1), adder.CarryOut);
+        }
+    }
+}
