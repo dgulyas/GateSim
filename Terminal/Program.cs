2e702cad99799aea1a7d3f38ba94d98a4f212dc8
David Gulyas <gulyman@gmail.com>
2023-01-04 12:12:48 -0700

    Change Terminal entry point into normal class

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GateSim;
+using GateSim.Simulations;
+
+namespace Terminal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sim = new Cpu01();
+            //var simState =
+        }
+
+    }
+}
